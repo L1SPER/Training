7d8943c8f6613ab2bdd88c6c4a170d337b274169
ALİM ARSLAN <alimarslan003@gmail.com>
2023-10-14 01:54:58 +0300

    Added Inventory Items

--- Training/Assets/Scripts/PlayerMovement.cs
+++ Training/Assets/Scripts/PlayerMovement.cs
@@ -1,9 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public static PlayerMovement instance;
+    private void Awake()
+    {
+        Singleton();
+    }
+
+    private void Singleton()
+    {
+        if(!instance)
+        {
+            instance = this;
+        }
+    }
+
     public CharacterController controller;
     [SerializeField] float speed;
     [SerializeField] float gravity = -9.81f;
@@ -14,28 +30,49 @@ public class PlayerMovement : MonoBehaviour
 
     Vector3 velocity;
     bool isGrounded;
+
+    public InventoryObject inventory;
     // Update is called once per frame
     void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)) 
+        {
+            inventory.Save();
+        }
+        if(Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            inventory.Load();
+        }
+        Movement();
+    }
+
+    private void Movement()
     {
         if (CheckGround() && velocity.y < 0f)
             velocity.y = -2f;
 
-        float x=Input.GetAxis("Horizontal");
+        float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-    
-        Vector3 move =transform.right*x+transform.forward*z;
-        controller.Move(move*speed*Time.deltaTime);
 
-        if(Input.GetButtonDown("Jump")&&CheckGround())
-        {
-            velocity.y=Mathf.Sqrt(jumpHeight*-2f*gravity);
-        }
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        //if (Input.GetButtonDown("Jump") && CheckGround())
+        //{
+        //    velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        //}
 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
     }
+
     bool CheckGround()
     {
         return isGrounded = Physics.CheckSphere(groundCheck.transform.position, groundDistance, groundMask);
+    }
+
+    private void OnApplicationQuit()
+    {
+        inventory.Container.Items= new InventorySlot[20];
     }
 }
